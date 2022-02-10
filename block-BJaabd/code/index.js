6b79661ed82ccacc2b21edb5c6395ba56c90b4d7
amruta dhawan <amrutadhawan@gmail.com>
2022-02-10 16:43:45 +0530

    javascript/assment level-3 ass1

--- block-BJaabd/code/index.js
+++ block-BJaabd/code/index.js
@@ -10,7 +10,20 @@ Write a program that asks the user his/her age and check for the following condi
 - `if` the age is greater than 55 then print the message " You are too old to participate in the marthon".
 👇
 */
-
+// Loops
+let age=prompt('enter age:');
+if(age>=12){
+  alert("You can participate in the marathon");
+}
+else if(age<=11){
+  alert('You are too young to participate in the marathon');
+}
+else if(age<=4){
+  alert('Hey Kiddo! Can You Walk ?');
+}
+else if(age>=55){
+  alert(' You are too old to participate in the marthon');
+}
 // Loops
 /*
 Given a positive integer `n`. Print the word (hello) in format of heeeello (letter 'e' must be repeated `n` times). Take input from prompt and print the result in alert.
@@ -21,12 +34,23 @@ n = 7 => output: heeeeeeello
 👇
 */
 // [Your code goes here]
+let a=prompt('enter letter:');
+let str='e';
+
+alert('output is:'+a+str.repeat(4));
 
 /*
 Program to calculate the sum of first n natural numbers(1,2,3...n are known as natural numbers). Prompt user to enter n (using `prompt`) then based on input provided calculate and show result in `alert`.
 👇
 */
 // [Your code goes here]
+const n=prompt('enter number:');
+let s=0;
+for(let i=1; i<=n;i++)
+{
+  s+=i;
+}
+alert('sum of natural numbers:'+s);
 
 /* Switch Statement
 
@@ -46,7 +70,38 @@ Take a number value from user and alert the message if it matches the conditions
 
 */
 // [Your code goes here]
-
+let num=prompt('enter number:');
+switch(true){
+  case num==1:
+    alert(num+ ' is equal to 1');
+    break;
+  case num==2:
+    alert(num+ ' is equal to 2');
+    break; 
+    case num==3:
+    alert(num+ ' is equal to 3');
+    break;
+    case num==4:
+    alert(num+ ' is equal to 4');
+    break;
+    case num==5:
+    alert(num+ ' is equal to 5');
+    break;
+    case num==6:
+    alert(num+ ' is equal to 6');
+    break;
+    case num==7:
+    alert(num+ ' is equal to 7');
+    break;
+    case num==8:
+    alert(num+ ' is equal to 8');
+    break;
+    case num==9:
+    alert(num+ ' is equal to 9');
+    break;
+    default:
+      alert(' PLEASE TRY AGAIN, if  is none of the above.') ;
+}
 /*
 🎖Using switch statement do the following
 
@@ -63,15 +118,51 @@ Take the value of `marks` (0-100) from user using `prompt` and `alert` the messa
 
 // [Your code goes here]
 
+let marks=prompt('enter marks:');
+switch(true){
+  case marks>90:
+    alert('AA');
+    case marks>=80:
+    alert('AB');
+    case marks>=70:
+    alert('BB');
+    case marks>=60:
+    alert('BC');
+    case marks>=50:
+    alert('CC');
+    case marks>=40:
+    alert('CD');
+    case marks>=30:
+    alert('DD');
+    case marks<=30:
+    alert('FF');
+}
+
+
 /*
  🎖Write a JavaScript program that takes two `integers` from user (using prompt) and alerts the larger number.
 */
 // [your code goes here]
+let num1=prompt('enter 1st number:');
+let num2=prompt('enter 2nd number:');
+if(num1>num2){
+  alert('greater number:'+num1);
+}
+else{
+  alert('greater number:'+num2);
+}
 
 /*
 🎖Write a JavaScript conditional statement to find the sign (+, -) of product of three numbers. Take those three numbers from user using `prompt`. Display an alert box with the specified sign.
 */
 // [Your code goes here]
+let x=prompt('enter value');
+let y=prompt('enter value');
+let z=prompt('enter value');
+let product=x*y*z;
+let sign=(product<0)? '-' : '+';
+alert(sign);
+
 
 /* Calculator
  🎖Make a simple calculator with these functions. Using prompt, type conversion, if else statement. Use prompt to take the input from user i.e two numbers and an operation (Add, Sub, Mul, Div).
@@ -86,3 +177,15 @@ Take the value of `marks` (0-100) from user using `prompt` and `alert` the messa
 */
 
 // [Your code goes here]
+let a=prompt('enter 1st number:');
+let b=prompt('enter 2nd number:');
+alert('addition:'+ a+b );
+alert('multiplication:'+ a*b);
+if(a<b){
+  alert('Number Two is larger then Number one');
+}
+else {
+  alert('substraction:'+ a-b);
+  alert('division:'+ a/b);
+}
+
